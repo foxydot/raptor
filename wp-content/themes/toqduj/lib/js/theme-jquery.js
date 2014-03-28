85f40db2bdcb8c4213d92986c53c8578a1ef9efa
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-03-28 11:22:45 -0400

    add external linking js

--- wp-content/themes/toqduj/lib/js/theme-jquery.js
+++ wp-content/themes/toqduj/lib/js/theme-jquery.js
@@ -28,3 +28,43 @@ jQuery(document).ready(function($) {
     $('.entry-content ul').not('.entry-content .gform_wrapper ul').addClass('fa-ul').find('li').prepend('<i class="fa-li fa fa-caret-right"></i>');
     $('.sidebar .widget_advanced_menu .menu li li a').prepend('<i class="fa fa-caret-right"></i>');
 });
+
+    jQuery(document).ready(function($) {    
+    // add target="_blank" to all *external* 
+    //create array of all *internal* urls that should NOT go to a separate window
+    var internal_urls = Array('raptorinc.org','www.raptorinc.org','raptor.msdlab2.com','127.0.0.1');
+    $('a').attr('target',function(){
+        var url = $(this).attr('href');
+        var target = $(this).attr('target');
+        if(url == '#' || strripos(url,'http',0)===false){
+            return '_self';
+        } else {
+            var i=0;
+            while (internal_urls[i]){
+                if(strripos(url, internal_urls[i], 0)){
+                    return target;
+                }
+                i++;
+            }
+            return '_blank';
+        }
+    });
+});
+function stripos(f_haystack, f_needle, f_offset) {
+  //  discuss at: http://phpjs.org/functions/stripos/
+  // original by: Martijn Wieringa
+  //  revised by: Onno Marsman
+  //   example 1: stripos('ABC', 'a');
+  //   returns 1: 0
+
+  var haystack = (f_haystack + '')
+    .toLowerCase();
+  var needle = (f_needle + '')
+    .toLowerCase();
+  var index = 0;
+
+  if ((index = haystack.indexOf(needle, f_offset)) !== -1) {
+    return index;
+  }
+  return false;
+}
